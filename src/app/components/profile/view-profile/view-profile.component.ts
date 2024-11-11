971deba969255157f3ad9faf9b4e3b9f26f79b87
claudiairgomes <1211619@isep.ipp.pt>
2024-11-11 12:20:45 +0000

    View Profile Component

---
+++ src/app/components/profile/view-profile/view-profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DataService } from '../../../services/data.service';
+
+@Component({
+  selector: 'app-view-profile',
+  standalone: true,
+  imports: [CommonModule, FormsModule], // Add HttpClientModule
+  templateUrl: './view-profile.component.html',
+  styleUrls: ['./view-profile.component.scss']
+})
+export class ViewProfileComponent implements OnInit {
+  profile: any;
+
+  dataService = inject(DataService);
+
+  ngOnInit(){
+    //this.animais = this.animaisService.getAnimais();
+    this.dataService.getProfile().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.profile = data;
+      },
+      error: (error) => {
+        console.log('Algo correu mal:', error);
+      }
+
+    });
+  }
+}
