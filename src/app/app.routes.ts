0a385716def206f20401a0da311c6e033fad67c8
Liliana <lilianasilvaalmeida@gmail.com>
2024-11-11 12:04:57 +0000

    Alterações gerais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GamesListComponent } from './components/games-list/games-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'games-list', component: GamesListComponent}
+
+
+];
