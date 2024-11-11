0caefbab15af911a5ab545886de57b8b3b775d0f
claudiairgomes <1211619@isep.ipp.pt>
2024-11-11 12:52:03 +0000

    Edit Profile Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { GamesListComponent } from './components/games-list/games-list.component';
+import { EditProfileComponent } from './components/profile/edit-profile/edit-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, RouterLink, RouterLinkActive, ViewProfileComponent, GamesListComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule, RouterLink, RouterLinkActive, ViewProfileComponent, GamesListComponent, EditProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ViewProfileComponent } from './components/profile/view-profile/view-profile.component';
 import { GamesListComponent } from './components/games-list/games-list.component';
+import { EditProfileComponent } from './components/profile/edit-profile/edit-profile.component';
 
 
 export const routes: Routes = [
-    { path: 'profile', component: ViewProfileComponent },     
+    { path: 'profile', component: ViewProfileComponent },   
+    { path: 'edit-profile', component: EditProfileComponent },  
     {path: 'games-list', component: GamesListComponent}
 ];
