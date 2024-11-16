6a5f552afd39500e9065362e61174300af9efbe4
MagsPeixoto <margaridapeixoto1996@gmail.com>
2024-11-16 21:27:05 +0000

    quase terminado

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { GamesList } from '../interfaces/games-list';
 import { FormsModule } from '@angular/forms';
 import { CommonModule, NgIf } from '@angular/common';
 
-
 @Component({
   selector: 'app-navigation',
   standalone: true,
   imports: [RouterLink, FormsModule, NgIf, CommonModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   searchTerm: string = '';  // Bind to the input field
   games: GamesList[] = [];  // Store the list of games
   filteredGames: GamesList[] = [];  // Store the filtered results
   isLoading: boolean = false;  // For loading state
+  userProfile: any = null;  // Store user profile info
 
   constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    // Carregar o perfil do usuário do localStorage
+  const savedProfile = localStorage.getItem('userProfile');
+  if (savedProfile) {
+    this.userProfile = JSON.parse(savedProfile);
+  }
+  }
 
   // Fetch games from the service and filter based on search term
   searchGames(): void {
@@ -41,9 +49,4 @@ export class NavigationComponent {
       }
     });
   }
-  
 }
-
-
-
-
